65aed7fb4876ce55acc058c20485b777563ce7cb
Nicolas Perussel <nicolas.perussel@ekino.com>
2018-11-26 20:22:40 +0100

    fix(schema): add last migration schema.

--- src/Migrations/Version20181126192122.php
+++ src/Migrations/Version20181126192122.php
@@ -8,20 +8,29 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181123164616 extends AbstractMigration
+final class Version20181126192122 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
+        $this->addSql('DROP INDEX IDX_64C19C1727ACA70');
+        $this->addSql('DROP INDEX UNIQ_64C19C1989D9B62');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__category AS SELECT id, parent_id, name, slug, sort_order FROM category');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, parent_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL COLLATE BINARY, slug VARCHAR(128) NOT NULL COLLATE BINARY, sort_order INTEGER NOT NULL, CONSTRAINT FK_64C19C1727ACA70 FOREIGN KEY (parent_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO category (id, parent_id, name, slug, sort_order) SELECT id, parent_id, name, slug, sort_order FROM __temp__category');
+        $this->addSql('DROP TABLE __temp__category');
+        $this->addSql('CREATE INDEX IDX_64C19C1727ACA70 ON category (parent_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_64C19C1989D9B62 ON category (slug)');
         $this->addSql('DROP INDEX IDX_CBE5A3317E3C61F9');
         $this->addSql('DROP INDEX UNIQ_CBE5A331989D9B62');
-        $this->addSql('CREATE TEMPORARY TABLE __temp__book AS SELECT id, owner_id, title, description, cover, isbn, authors, slug FROM book');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__book AS SELECT id, owner_id, title, description, cover, isbn, authors, slug, comments, location FROM book');
         $this->addSql('DROP TABLE book');
         $this->addSql('CREATE TABLE book (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, owner_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL COLLATE BINARY, description CLOB NOT NULL COLLATE BINARY, cover VARCHAR(255) NOT NULL COLLATE BINARY, isbn VARCHAR(13) NOT NULL COLLATE BINARY, authors CLOB NOT NULL COLLATE BINARY --(DC2Type:array)
-        , slug VARCHAR(128) NOT NULL COLLATE BINARY, comments CLOB DEFAULT NULL, location VARCHAR(40) DEFAULT NULL, CONSTRAINT FK_CBE5A3317E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
-        $this->addSql('INSERT INTO book (id, owner_id, title, description, cover, isbn, authors, slug) SELECT id, owner_id, title, description, cover, isbn, authors, slug FROM __temp__book');
+        , slug VARCHAR(128) NOT NULL COLLATE BINARY, comments CLOB DEFAULT NULL COLLATE BINARY, location VARCHAR(40) DEFAULT NULL COLLATE BINARY, CONSTRAINT FK_CBE5A3317E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO book (id, owner_id, title, description, cover, isbn, authors, slug, comments, location) SELECT id, owner_id, title, description, cover, isbn, authors, slug, comments, location FROM __temp__book');
         $this->addSql('DROP TABLE __temp__book');
         $this->addSql('CREATE INDEX IDX_CBE5A3317E3C61F9 ON book (owner_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_CBE5A331989D9B62 ON book (slug)');
@@ -43,15 +52,6 @@ final class Version20181123164616 extends AbstractMigration
         $this->addSql('DROP TABLE __temp__borrowed_book');
         $this->addSql('CREATE INDEX IDX_50A9B8BCA76ED395 ON borrowed_book (user_id)');
         $this->addSql('CREATE INDEX IDX_50A9B8BC16A2B381 ON borrowed_book (book_id)');
-        $this->addSql('DROP INDEX IDX_64C19C1727ACA70');
-        $this->addSql('DROP INDEX UNIQ_64C19C1989D9B62');
-        $this->addSql('CREATE TEMPORARY TABLE __temp__category AS SELECT id, parent_id, name, slug, sort_order FROM category');
-        $this->addSql('DROP TABLE category');
-        $this->addSql('CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, parent_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL COLLATE BINARY, slug VARCHAR(128) NOT NULL COLLATE BINARY, sort_order INTEGER NOT NULL, CONSTRAINT FK_64C19C1727ACA70 FOREIGN KEY (parent_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
-        $this->addSql('INSERT INTO category (id, parent_id, name, slug, sort_order) SELECT id, parent_id, name, slug, sort_order FROM __temp__category');
-        $this->addSql('DROP TABLE __temp__category');
-        $this->addSql('CREATE INDEX IDX_64C19C1727ACA70 ON category (parent_id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_64C19C1989D9B62 ON category (slug)');
     }
 
     public function down(Schema $schema) : void
@@ -61,11 +61,11 @@ final class Version20181123164616 extends AbstractMigration
 
         $this->addSql('DROP INDEX UNIQ_CBE5A331989D9B62');
         $this->addSql('DROP INDEX IDX_CBE5A3317E3C61F9');
-        $this->addSql('CREATE TEMPORARY TABLE __temp__book AS SELECT id, owner_id, title, description, cover, isbn, authors, slug FROM book');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__book AS SELECT id, owner_id, title, description, cover, isbn, authors, slug, comments, location FROM book');
         $this->addSql('DROP TABLE book');
         $this->addSql('CREATE TABLE book (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, owner_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL, description CLOB NOT NULL, cover VARCHAR(255) NOT NULL, isbn VARCHAR(13) NOT NULL, authors CLOB NOT NULL --(DC2Type:array)
-        , slug VARCHAR(128) NOT NULL)');
-        $this->addSql('INSERT INTO book (id, owner_id, title, description, cover, isbn, authors, slug) SELECT id, owner_id, title, description, cover, isbn, authors, slug FROM __temp__book');
+        , slug VARCHAR(128) NOT NULL, comments CLOB DEFAULT NULL, location VARCHAR(40) DEFAULT NULL)');
+        $this->addSql('INSERT INTO book (id, owner_id, title, description, cover, isbn, authors, slug, comments, location) SELECT id, owner_id, title, description, cover, isbn, authors, slug, comments, location FROM __temp__book');
         $this->addSql('DROP TABLE __temp__book');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_CBE5A331989D9B62 ON book (slug)');
         $this->addSql('CREATE INDEX IDX_CBE5A3317E3C61F9 ON book (owner_id)');
